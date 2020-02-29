a1f52a5729ae1c7268d6f0d26cf21dfa8f4e3842
Md Forhad Sarkar <news9097@gmail.com>
2020-02-29 16:59:33 +0800

    Updates

---
+++ precache-manifest.c779156033c85612099e6ec571b2fd9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cb7bb2353bbcd25b289cc52d24ef022",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "2dee7659246fdf6b0580",
+    "url": "/calculator/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1220d6816a2d9f7b5df4",
+    "url": "/calculator/static/js/2.51b6205e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/calculator/static/js/2.51b6205e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dee7659246fdf6b0580",
+    "url": "/calculator/static/js/main.8b1b6d3b.chunk.js"
+  },
+  {
+    "revision": "ccfc56ce1b0c91cb76b8",
+    "url": "/calculator/static/js/runtime-main.627f4c10.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
