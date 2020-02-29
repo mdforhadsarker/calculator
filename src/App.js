13e3e31eda990af40ff8ef1c508cb46a41557f1a
Md Forhad Sarkar <news9097@gmail.com>
2020-02-29 17:05:11 +0800

    Customized

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import React from 'react';
-import logo from './logo.svg';
+import profileImage from './img/forhad.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Welcome to React App
+      </h1>
+      <h2>Author: Md Forhad Sarkar</h2>
+      <img style={{width:400}} src={profileImage} alt="profile-image"/>
     </div>
   );
 }
